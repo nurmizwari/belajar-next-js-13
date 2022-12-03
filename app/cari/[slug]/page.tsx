dd498984435abdf56af4934a62906b9c33af0aec
nur mizwari <nurmizwari04@gmail.com>
2022-12-03 22:15:15 +0700

    sequential data fetching

--- app/cari/[slug]/page.tsx
+++ app/cari/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 async function getData(param: string) {
   const res = await fetch(`https://api.github.com/search/users?q=${param}`);
@@ -9,24 +9,39 @@ async function getDataRepos(param: string) {
   return res.json();
 }
 
+const RepoList = async ({ slug }: any) => {
+  const dataRepo = await getDataRepos(slug);
+  return (
+    <div>
+      <p>List Repository</p>
+      <div>{JSON.stringify(dataRepo)}</div>
+    </div>
+  );
+};
+
 export default async function DetailCari({
   params,
 }: {
   params: { slug: string };
 }) {
   const dataUser = await getData(params.slug);
-  const dataRepo = await getDataRepos(params.slug);
 
-  const [user, repo] = await Promise.all([dataUser, dataRepo]); // gini juga bisa tanpa ini juga bisa
+  // const [user, repo] = await Promise.all([dataUser, dataRepo]); // gini juga bisa tanpa ini juga bisa
   // langsung pake dataUser juga bisa tanpa perlu await promise
   //! INI BELAJAR PARALLER DATA FETCHING (FETCH DATA BERBARENGAN)
 
   return (
     <>
       <p>Detail User : {params.slug}</p>
-      <div>{JSON.stringify(user)}</div>
-      <p>List Repository : {params.slug}</p>
-      <div>{JSON.stringify(repo)}</div>
+      <div>{JSON.stringify(dataUser)}</div>
+      <div style={{ marginTop: "10px" }}>
+        <Suspense fallback={<div>wait ..</div>}>
+          {/* @ts-ignore */}
+          <RepoList slug={params.slug} />
+        </Suspense>
+      </div>
+      {/* <p>List Repository : {params.slug}</p>
+      <div>{JSON.stringify(repo)}</div> */}
     </>
   );
 }
