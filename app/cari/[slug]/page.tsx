c6fae7a14571ef268b95aadb1deeeb959a5d5e40
nur mizwari <nurmizwari04@gmail.com>
2022-12-03 22:02:44 +0700

    belajar parallel data fetching

--- app/cari/[slug]/page.tsx
+++ app/cari/[slug]/page.tsx
@@ -4,17 +4,29 @@ async function getData(param: string) {
   const res = await fetch(`https://api.github.com/search/users?q=${param}`);
   return res.json();
 }
+async function getDataRepos(param: string) {
+  const res = await fetch(`https://api.github.com/users/${param}/repos`);
+  return res.json();
+}
 
 export default async function DetailCari({
   params,
 }: {
   params: { slug: string };
 }) {
-  const data = await getData(params.slug);
+  const dataUser = await getData(params.slug);
+  const dataRepo = await getDataRepos(params.slug);
+
+  const [user, repo] = await Promise.all([dataUser, dataRepo]); // gini juga bisa tanpa ini juga bisa
+  // langsung pake dataUser juga bisa tanpa perlu await promise
+  //! INI BELAJAR PARALLER DATA FETCHING (FETCH DATA BERBARENGAN)
+
   return (
     <>
       <p>Detail User : {params.slug}</p>
-      <div>{JSON.stringify(data)}</div>
+      <div>{JSON.stringify(user)}</div>
+      <p>List Repository : {params.slug}</p>
+      <div>{JSON.stringify(repo)}</div>
     </>
   );
 }
